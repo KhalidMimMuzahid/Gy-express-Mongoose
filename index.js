c5eb2ac686274ca2bd199c490d7eed84a5586345
Himanshu Singh <vertigo.himanshu@gmail.com>
2023-11-25 03:52:11 +0000

    some changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const port = 2223;
+const port = 2023;
 
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const publicRoutes = require("./routes/publicRoutes/index");
