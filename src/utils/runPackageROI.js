b4cbedb967ef961a30512775983752d4791157c1
Abul Bashar <bashar@triplewsols.org>
2023-12-28 17:44:05 +0600

    feat: ROI income issue solved

--- src/utils/runPackageROI.js
+++ src/utils/runPackageROI.js
@@ -13,70 +13,80 @@ const runPackageROI = () => {
     // "*/2 * * * *", // Every 02 mins
     // "*/30 * * * * *", // every 05 secs
     async () => {
-      const roiPercentage = await RoiSetting.findOne({});
-      const today = new Date(getIstTime().date).toDateString().split(" ")[0];
-      if (today === "Sat" || today === "Wed") {
-        return res.status(400).json({
-          message: "ROI isn't distributed on Saturday and Sunday",
-        });
-      }
-      const extRoi = await PackageRoi.find({});
-      for (const ext of extRoi) {
-        const wallet = await Wallet.findOne({ userId: ext.userId });
-        // Regular ROI code
-        if (ext.isActive) {
-          console.log("userid", ext.userId);
-          const incomeDayInc = ext.incomeDay + 1;
-          const currentPackageAmount =
-            ((ext.currentPackage + wallet?.activeIncome) / 100) *
-            roiPercentage?.roiPercentage;
-
-          const roiPerDayCommissionAmount = currentPackageAmount;
-          // console.log(roiPerDayCommissionAmount);
-          const roiPerDayCommissionPercentage = roiPercentage?.roiPercentage;
-          await PackageRoi.findOneAndUpdate(
-            { packageId: ext.packageId },
-            {
-              $inc: {
-                incomeDay: +1,
-                totalReturnedAmount: +roiPerDayCommissionAmount,
-              },
-              $push: {
-                history: {
-                  userId: ext.userId,
-                  fullName: ext.fullName,
-                  package: ext.currentPackage,
-                  commissionPercentagePerDay: roiPerDayCommissionPercentage,
-                  commissionAmount: Number(roiPerDayCommissionAmount).toFixed(
-                    4
-                  ),
-                  totalCommissionAmount: Number(
-                    ext.totalReturnedAmount + roiPerDayCommissionAmount
-                  ).toFixed(4),
-                  incomeDay: incomeDayInc,
-                  incomeDate: new Date(getIstTime().date).toDateString(),
-                  incomeTime: getIstTime().time,
-                  incomeDateInt: new Date(getIstTime().date).getTime(),
-                  transactionId: generateRandomString(),
-                },
-              },
-            }
-          );
-          await Wallet.findOneAndUpdate(
-            { userId: ext.userId },
-            {
-              $inc: {
-                roiIncome: +roiPerDayCommissionAmount,
-                totalIncome: +roiPerDayCommissionAmount,
-                activeIncome: +roiPerDayCommissionAmount,
-              },
-            }
-          );
-          // // level income distribute
-          await levelIncome(ext, roiPerDayCommissionAmount);
+      try {
+        const roiPercentage = await RoiSetting.findOne({});
+        const today = new Date(getIstTime().date).toDateString().split(" ")[0];
+        if (today === "Sat" || today === "Sun") {
+          return res.status(400).json({
+            message: "ROI isn't distributed on Saturday and Sunday",
+          });
         }
+        const extRoi = await PackageRoi.find({});
+        for (const ext of extRoi) {
+          const wallet = await Wallet.findOne({ userId: ext.userId });
+          // Regular ROI code
+          if (ext.isActive) {
+            console.log("userid", ext.userId);
+            const incomeDayInc = ext.incomeDay + 1;
+            const currentPackageAmount =
+              ((Number(ext.currentPackage) + Number(wallet?.activeIncome)) /
+                100) *
+              Number(roiPercentage?.roiPercentage);
+
+            const roiPerDayCommissionAmount = currentPackageAmount;
+            // console.log(roiPerDayCommissionAmount);
+            const roiPerDayCommissionPercentage = Number(
+              roiPercentage?.roiPercentage
+            );
+            await PackageRoi.findOneAndUpdate(
+              { packageId: ext.packageId },
+              {
+                $inc: {
+                  incomeDay: +1,
+                  totalReturnedAmount: +Number(roiPerDayCommissionAmount),
+                },
+                $push: {
+                  history: {
+                    userId: ext.userId,
+                    fullName: ext.fullName,
+                    package: ext.currentPackage,
+                    commissionPercentagePerDay: Number(
+                      roiPerDayCommissionPercentage
+                    ),
+                    commissionAmount: Number(roiPerDayCommissionAmount).toFixed(
+                      4
+                    ),
+                    totalCommissionAmount: Number(
+                      Number(ext.totalReturnedAmount) +
+                        Number(roiPerDayCommissionAmount)
+                    ).toFixed(4),
+                    incomeDay: Number(incomeDayInc),
+                    incomeDate: new Date(getIstTime().date).toDateString(),
+                    incomeTime: getIstTime().time,
+                    incomeDateInt: new Date(getIstTime().date).getTime(),
+                    transactionId: generateRandomString(),
+                  },
+                },
+              }
+            );
+            await Wallet.findOneAndUpdate(
+              { userId: ext.userId },
+              {
+                $inc: {
+                  roiIncome: +Number(roiPerDayCommissionAmount),
+                  totalIncome: +Number(roiPerDayCommissionAmount),
+                  activeIncome: +Number(roiPerDayCommissionAmount),
+                },
+              }
+            );
+            // // level income distribute
+            await levelIncome(ext, Number(roiPerDayCommissionAmount));
+          }
+        }
+        console.log("Distribue roi");
+      } catch (error) {
+        console.log("error", error);
       }
-      console.log("Distribue roi");
     },
     { scheduled: true, timezone: "Asia/Kolkata" }
   );
